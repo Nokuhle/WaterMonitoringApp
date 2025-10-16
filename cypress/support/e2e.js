059d79e3258c5b70bb994494be84259eaa85a378
Nokuhle <ngubanenokuhle@gmail.com>
2025-10-16 13:42:49 +0200

    [FIXED]Folder Structure

--- cypress/support/e2e.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="cypress" />
-
-Cypress.Commands.add('login', (email, password) => {
-  cy.visit('/login.html')
-  cy.get('#loginEmail').type(email)
-  cy.get('#loginPassword').type(password)
-  cy.get('form').submit()
-})
--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
-  moduleNameMapper: {
-    '^chart.js$': '<rootDir>/node_modules/chart.js/dist/chart.min.js'
-  }
-}
